1b0dca2aaa6116f9c626474a0826dc9b7aa05597
Yuvadi29 <adityatrivedi612@gmail.com>
2024-07-07 22:28:05 +0530

    NodeJS Day 1

---
+++ 01-Introduction/01-Introduction.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+console.log("Hey There, Subscribe to Coding Adda");
+
+let channel = "Youtube";
+console.log(channel);
+
+// alert("Hey , You are Aditya!!!");
+
+// prompt("Enter your name please: ");
