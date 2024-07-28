536fe219943be7ed06f6d82eddd11fa02dab7dd6
Yuvadi29 <adityatrivedi612@gmail.com>
2024-07-28 18:06:11 +0530

    NodeJs Day 6

--- 05-Server/index.js
+++ 05-Server/index.js
@@ -1,19 +1,39 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-    console.log("Request from Client Received");
-    console.log(req.headers);
+    // console.log("Request from Client Received");
+    // console.log(req.headers);
     // console.log(req.httpVersion);
-    const text = "Hello from Client";
-    fs.writeFile("newText.txt", text, (err,data) => {
-        if(err) console.log(err);
-        return data;
+    if(req.url === "/favicon.ico") return res.end();
+    const text = `${Date.now()}: ${req.url} Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+    fs.appendFile("newText.txt", text, (err, data) => {
+        switch (myUrl.pathname) {
+            case "/":
+                res.end("Hello Adi");
+                break;
+
+            case "/about":
+                const username = myUrl.query.myName;
+                res.end(`Hello ${username}`);
+                break;
+
+            case "/help":
+                res.end("Helppppp!!!");
+
+            default:
+                res.end("Not Found")
+        }
+        // if (err) console.log(err);
+        // return data;
     });
 
-    res.end("Hello from the Server");
+    // res.end("Hello from the Server");
 });
 
-myServer.listen(5000, () =>{
+myServer.listen(5000, () => {
     console.log('Server Running');
 });
